c3ded277669cc9dc020b2af6ab059ab38643b392
Dev Singhal <singhaldev26@gmail.com>
2024-03-12 19:02:46 +0530

    init commit

---
+++ app/src/main/java/com/example/f1game/data/guidelines.kt
@@ -0,0 +1,10 @@
+package com.example.f1game.data
+
+data class guidelines(
+    val guide : String = ""
+)
+val listOfguide= listOf(
+    guidelines("If you use hint,10 points will be deducted"),
+    guidelines("One player at a time"),
+    guidelines("Maximum Achievable Score is 200")
+)
